7f5813bf6e524f5397182cf9bc48ad15ba7dc62b
Andyrei <radoacaandrei3@gmail.com>
2025-02-01 22:58:33 +0100

    EDIT: settings page to run build

--- src/app/(pages)/settings/page.tsx
+++ src/app/(pages)/settings/page.tsx
@@ -1,9 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-type Props = {}
-
-export default function page({}: Props) {
+export default function page() {
   return (<div className='mx-auto max-w-screen-sm min-h-[100dvh] flex flex-col items-center justify-center'>
     <h1> THIS PAGE IS A WORK IN PROGRESS!!!!</h1>
     <Link href={'/'} className='text-blue-700 text-2xl underline'> &lt;&lt; Go back HOME</Link>
