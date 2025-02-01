3d6ed56a6357979005c77ad6a912d1bc18f82a79
Andyrei <radoacaandrei3@gmail.com>
2025-02-01 21:01:49 +0100

    EDIT: eslint for DEMO PURPOSES

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      
+      // NO explicit any IS ONLY FOR DEMO PURPOSES
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }),
+  
 ];
 
 export default eslintConfig;
