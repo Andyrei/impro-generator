e7dbef4c937ac904709165aec24e5ab1fdf95e1c
Andyrei <radoacaandrei3@gmail.com>
2025-02-02 18:13:13 +0100

    EDIT: V0.0.1 -> V0.0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <div className="mx-auto max-w-screen-sm min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
       <header className="bg-red-800 relative flex overflow-x-hidden">
         <div className="animate-marquee text-3xl font-bold text-center whitespace-nowrap">
-          !!!! VERSIONE ALPHA - V0.0.1 ANCORA IN SVILUPPO !!!!
+          !!!! VERSIONE ALPHA - V0.0.2 ANCORA IN SVILUPPO !!!!
         </div>
         <div className="absolute top-0 animate-marquee2 text-3xl font-bold text-center whitespace-nowrap">
-          !!!! VERSIONE ALPHA - V0.0.1 ANCORA IN SVILUPPO !!!!
+          !!!! VERSIONE ALPHA - V0.0.2 ANCORA IN SVILUPPO !!!!
         </div>
       </header>
       
