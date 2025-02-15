90548e856e618a685316306881f2cfa9e9201bae
Andyrei <radoacaandrei3@gmail.com>
2025-02-16 00:09:12 +0100

    EDIT: make it more automatic with less places to edit

--- src/app/[lang]/getDictionary.ts
+++ src/app/[lang]/getDictionary.ts
@@ -1,10 +1,30 @@
 export type LocaleType = keyof typeof dictionaries;
 
-const dictionaries = {
-    en: () => import('./dictionaries/en.json').then((module) => module.default),
-    ro: () => import('./dictionaries/ro.json').then((module) => module.default),
-    it: () => import('./dictionaries/it.json').then((module) => module.default),
-};
+export const languages = [
+    {
+        code: 'en',
+        title: 'English'
+    },
+    {
+        code: 'it',
+        title: "Italian"
+    },
+    {
+        code: 'ro',
+        title: 'Romanian'
+    }
+]
+
+const dictionaries = languages.reduce((acc, lang) => {
+    acc[lang.code] = () => import(`./dictionaries/${lang.code}.json`).then((module) => module.default);
+    return acc;
+}, {} as { [key: string]: () => Promise<any> });
+
+// const dictionaries = {
+//     en: () => import('./dictionaries/en.json').then((module) => module.default),
+//     ro: () => import('./dictionaries/ro.json').then((module) => module.default),
+//     it: () => import('./dictionaries/it.json').then((module) => module.default),
+// };
 
 // Returns the dictionary corresponding to the locale
 export const getDictionary = async (locale: LocaleType) => {
