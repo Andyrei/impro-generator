2e8090784490d9a3cbf2d28c1af93e2ff596b838
Andyrei <radoacaandrei3@gmail.com>
2026-03-02 21:19:32 +0100

    adding theme context into layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,9 +1,10 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/app/globals.css";
+import "@/app/[lang]/globals.css";
 import { LocaleProvider } from "../../context/LocaleContext";
 import Navbar from "@/components/custom-ui/Navbar";
+import ClientThemeProvider from "@/context/ThemeContext";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -33,20 +34,22 @@ export default async function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
                 <LocaleProvider initialLocale={lang}>
-                    <div className="mx-auto max-w-screen-sm min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
-                        <header className="bg-red-800 relative flex overflow-x-hidden">
-                            <div className="animate-marquee text-3xl font-bold text-center whitespace-nowrap">
-                                !!!! VERSIONE ALPHA - V0.0.4 ANCORA IN SVILUPPO!!!!
-                            </div>
-                            <div className="absolute top-0 animate-marquee2 text-3xl font-bold text-center whitespace-nowrap">
-                                !!!! VERSIONE ALPHA - V0.0.4 ANCORA IN SVILUPPO!!!!
-                            </div>
-                        </header>
-                        <main className="p-5">
-                          {children}
-                        </main>
-                        <Navbar />
-                    </div>
+                    <ClientThemeProvider>
+                        <div className="mx-auto max-w-screen-sm min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
+                            <header className="bg-red-800 relative flex overflow-x-hidden">
+                                <div className="animate-marquee text-3xl font-bold text-center whitespace-nowrap">
+                                    !!!! VERSIONE ALPHA - V0.0.4 ANCORA IN SVILUPPO!!!!
+                                </div>
+                                <div className="absolute top-0 animate-marquee2 text-3xl font-bold text-center whitespace-nowrap">
+                                    !!!! VERSIONE ALPHA - V0.0.4 ANCORA IN SVILUPPO!!!!
+                                </div>
+                            </header>
+                            <main className="p-5">
+                                {children}
+                            </main>
+                            <Navbar />
+                        </div>
+                    </ClientThemeProvider>
                 </LocaleProvider>
                 <Analytics />
             </body>
