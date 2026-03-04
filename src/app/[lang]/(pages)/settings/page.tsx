21db5536bd00a08ea32b4cced5ef802fdb78cca4
Andyrei <radoacaandrei3@gmail.com>
2026-03-04 19:40:49 +0100

    EDIT: fix ui on settings

--- src/app/[lang]/(pages)/settings/page.tsx
+++ src/app/[lang]/(pages)/settings/page.tsx
@@ -1,17 +1,16 @@
 import LanguageSelector from '@/components/custom-ui/LanguageSelector'
-import Link from 'next/link'
-import React from 'react'
 
 export default function page() {
-  return (<div>
+  return (<div  className="mx-5">
     <h1 className="text-3xl font-bold text-center">Settings</h1>
     <div className=''>
       <h2 className='text-xl font-bold my-5'>Language settings</h2>
 
-      <div className=''>
+      <div className='flex align-middle items-center gap-5'>
         <p>Select Language </p>
         <LanguageSelector />
       </div>
     </div>
-</div>)
+</div>
+)
 }
