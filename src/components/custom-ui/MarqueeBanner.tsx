e19567ac021284df557caac180f951e2e2b8298a
Andyrei <radoacaandrei3@gmail.com>
2026-03-07 17:07:22 +0100

    feat: update MarqueeBanner component to enhance text visibility with white color

--- src/components/custom-ui/MarqueeBanner.tsx
+++ src/components/custom-ui/MarqueeBanner.tsx
@@ -6,7 +6,7 @@ export default function MarqueeBanner() {
   const pathname = usePathname();
   if (pathname === '/it' || pathname === '/en') return (<>
       {[1, 2].map((n) => (
-        <div key={n} className="animate-marquee text-lg md:text-3xl font-bold text-center whitespace-nowrap">
+        <div key={n} className="animate-marquee text-lg md:text-3xl font-bold text-center text-white whitespace-nowrap">
           !!!! VERSIONE ALPHA - V0.0.6 ANCORA IN SVILUPPO !!!!
         </div>
       ))}
